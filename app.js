f337da1d93afbebc9877f4c684d2c33fcaa419c1
PlayeredLC <playered.lc@gmail.com>
2021-01-05 22:02:57 -0300

    Add a ejs template and listing prototype.

--- app.js
+++ app.js
@@ -4,9 +4,30 @@ const bodyParser = require("body-parser");
 
 const PORT = 3000;
 const app = express();
+const today = new Date();
+var items = ["Do something", "Do something else"];
+
+var options = {
+  month: "long",
+  day: "numeric",
+  weekday: "short",
+  year: "numeric",
+};
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
-  res.send("Server is up and running.");
+  var date = today.toLocaleDateString("en-US", options); 
+  res.render("list", {
+    date: date,
+    item: items
+  });
+});
+
+app.post("/", (req, res) => {
+  items.push(req.body.newItem);
+  res.redirect("/");
 });
 
 app.listen(PORT, () => {
