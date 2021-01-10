f224f871e90ca7d23d59d4d3758d8563beb3ad55
PlayeredLC <playered.lc@gmail.com>
2021-01-10 12:56:08 -0300

    Display tasks from db on main list.

--- app.js
+++ app.js
@@ -6,8 +6,9 @@ const date = require(__dirname+'/date.js');
 
 const app = express();
 const PORT = 3000;
-const quickList = ["Do something"];
+const quickList = [];
 const persList = [];
+const defaultData = [];
 
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({extended: true}));
@@ -15,34 +16,53 @@ app.set('view engine', 'ejs');
 
 //CONNECT TO DB
 mongoose.connect('mongodb://localhost:27017/tasklDB', {useNewUrlParser: true, useUnifiedTopology: true});
+
 const itemSchema = new mongoose.Schema({
   //(docs) If you want to add additional keys later, use the Schema#add method.
   task: String,
   active: Boolean
 });
 const Item = mongoose.model('Item', itemSchema);
-//LOAD FROM DB
-Item.find((err, itens) => {
-  if (err){
-    console.log(err);
-  }else{
-    itens.forEach(element => {
-      quickList.push(element.task);
-      console.log(quickList);
-    });
-  }
-});
+
+//DEFAULT DATA
+defaultData.push(new Item({
+  task: 'Welcome to your TASKList',
+  active: true
+}));
+defaultData.push(new Item({
+  task: 'Hit the + button to add a new task',
+  active: true
+}));
+defaultData.push(new Item({
+  task: 'Use the checkbox to discard accomplished tasks.',
+  active: true
+}));
 
 // ROOT ROUTE
 app.get('/', (req, res) => {
   let listTitle = 'Quick List';
-  
-  res.render('list', {
-    date: date.getDate(),
-    listTitle: listTitle,
-    list: quickList
+  Item.find({}, (err, items) => {
+    console.log(items.length);
+    if(items.length === 0){
+      //insert default
+      Item.insertMany(defaultData, (err) => {
+        if(err){
+          console.log(err);
+        }else{
+          console.log('Succesfully inserted default values.');
+        }
+      });
+      res.redirect('/');
+    }else{
+      res.render('list', {
+        date: date.getDate(),
+        listTitle: listTitle,
+        list: items
+      });
+    }
   });
 });
+
 app.post('/', (req, res) => {
   if(req.body.list === 'Personal'){
     persList.push(req.body.newItem);
@@ -66,27 +86,3 @@ app.get('/Personal', (req, res) => {
 app.listen(PORT, () => {
   console.log('started at port 3000..');
 });
-
-// EXAMPLE DATA
-// const data = [
-//   {
-//     task: 'Buy food',
-//     active: true
-//   },
-//   {
-//     task: 'Cook food',
-//     active: true
-//   },
-//   {
-//     task: 'Eat food',
-//     active: true
-//   }
-// ];
-//INSERT DATA
-// Item.insertMany(data, (err) => {
-//   if(err){
-//     console.log(err);
-//   }else{
-//     console.log('Successfuly added.');
-//   }
-// });
