44b631a71579ff150e90b3acebe58e5ca972ee34
PlayeredLC <playered.lc@gmail.com>
2021-01-27 01:56:28 -0300

    Change data modelings to add user accounts.

--- app.js
+++ app.js
@@ -16,77 +16,73 @@ app.set('view engine', 'ejs');
 
 //CONNECT TO DB
 mongoose.connect('mongodb+srv://'+ process.env.DB_USER +':'+ process.env.DB_PASS +'@cluster0.k8whr.mongodb.net/'+process.env.DB_NAME+'?retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology: true});
-const itemSchema = new mongoose.Schema({
-  //(docs) If you want to add additional keys later, use the Schema#add method.
-  task: {
-    type: String, 
-    required: true
-  },
-  list: {
-    type: String, 
-    required: true
-  }
-});
-const Item = mongoose.model('Item', itemSchema);
 
-//DEFAULT DATA
-defaultData.push(new Item({task: 'Welcome to your TASKList', list: 'Quick list'}));
-defaultData.push(new Item({task: 'Hit the + button to add a new task', list: 'Quick list'}));
-defaultData.push(new Item({task: 'Use the checkbox to discard accomplished tasks.', list: 'Quick list'}));
+const listSchema = new mongoose.Schema({
+  name: String,
+  items: Array
+});
+const userSchema = new mongoose.Schema({
+  username: String,
+  lists: [listSchema]
+});
+const User = mongoose.model('User', userSchema);
+const List = mongoose.model('List', listSchema);
+
+const defaultList = new List({
+  name: 'Quick List',
+  items: ['Welcome to your TASKList', 'Hit the + button to add a new task', 'Use the checkbox to discard accomplished tasks.']
+});
 
 // ROOT ROUTE
 app.get('/', (req, res) => {
-  let listTitle = 'Quick list';
-  Item.find({list: listTitle}, (err, items) => {
-    if(items.length === 0){
-      //insert default
-      Item.insertMany(defaultData, (err) => {
-        if(!err){
-          res.redirect('/');
-        }
-      });
-    }else{
+  
+  User.find({username: 'playeredlc'}, (err, user) => {
+    if(!err){
+      const foundUser = user[0];
       res.render('list', {
         date: date.getDate(),
-        listTitle: listTitle,
-        list: items
+        listTitle: foundUser.lists[0].name,
+        list: foundUser.lists[0].items  
       });
+    }else{
+      console.log(err);
     }
   });
+
 });
 
 //DYNAMIC ROUTING FOR OTHER LISTS.
-app.get('/:listName', (req, res) => {
-  let listTitle = _.capitalize(req.params.listName);
-  Item.find({list: listTitle}, (err, items) => {
-    if(!err){
-      res.render('list',{
-        date: date.getDate(),
-        listTitle: listTitle,
-        list: items
-      });      
-    }
-  });
-});
+// app.get('/:listName', (req, res) => {
+//   let listTitle = _.capitalize(req.params.listName);
+//   Item.find({list: listTitle}, (err, items) => {
+//     if(!err){
+//       res.render('list',{
+//         date: date.getDate(),
+//         listTitle: listTitle,
+//         list: items
+//       });      
+//     }
+//   });
+// });
 
-//HANDLE NEW TASKS BEING ADDED.
-app.post('/', (req, res) => {
-  const item = new Item({
-    task: req.body.newItem,
-    list: req.body.list
-  });
-  item.save();
-  res.redirect('/'+req.body.list);
-});
+// //HANDLE NEW TASKS BEING ADDED.
+// app.post('/', (req, res) => {
+//   const item = new Item({
+//     task: req.body.newItem,
+//     list: req.body.list
+//   });
+//   item.save();
+//   res.redirect('/'+req.body.list);
+// });
 
-//HANDLE DELETIONS
-app.post('/delete', (req, res) => {
-  Item.findByIdAndDelete(req.body.checkbox, (err) => {
-    if(!err){
-      res.redirect('/'+req.body.listName);
-    }  
-  });
-});
+// //HANDLE DELETIONS
+// app.post('/delete', (req, res) => {
+//   Item.findByIdAndDelete(req.body.checkbox, (err) => {
+//     if(!err){
+//       res.redirect('/'+req.body.listName);
+//     }  
+//   });
+// });
 
 if(port == null || port == ''){
   port=3000;
