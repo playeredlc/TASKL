ed9960c47d5bb673cc54219833e94fa5bdf43f70
PlayeredLC <playered.lc@gmail.com>
2021-01-27 22:28:03 -0300

    Setup passport local and OAuth2.0 authentication.

--- app.js
+++ app.js
@@ -4,15 +4,30 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const _ = require('lodash');
 const date = require(__dirname+'/date.js');
+const session = require('express-session');
+const passport = require('passport');
+const passportLocalMongoose = require('passport-local-mongoose');
+const findOrCreate = require('mongoose-findorcreate');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const LocalStrategy = require('passport-local').Strategy;
 
 const app = express();
 let port = process.env.PORT;
-const persList = [];
-// const defaultData = [];
 
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
+
+//express-session config
+app.use(session({
+  secret: process.env.SECRET_STR,
+  resave: false,
+  saveUninitialized: false,
+  cookie: {maxAge: 31556952000} //31556952000ms = 1 year
+}));
+//initialize passport
+app.use(passport.initialize());
+app.use(passport.session());
 
 //CONNECT TO DB
 mongoose.connect('mongodb+srv://'+ process.env.DB_USER +':'+ process.env.DB_PASS +'@cluster0.k8whr.mongodb.net/'+process.env.DB_NAME+'?retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology: true});
@@ -25,8 +40,41 @@ const userSchema = new mongoose.Schema({
   username: String,
   lists: [listSchema]
 });
+userSchema.plugin(passportLocalMongoose);
+userSchema.plugin(findOrCreate);
+
 const User = mongoose.model('User', userSchema);
 const List = mongoose.model('List', listSchema);
+
+//Set authentication strategies
+//local: username/password
+passport.use(new LocalStrategy(
+  function(username, password, done) {
+    User.findOne({ username: username }, function(err, user) {
+      if (err) { return done(err); }
+      if (!user) {
+        return done(null, false, { message: 'Incorrect username.' });
+      }
+      if (!user.validPassword(password)) {
+        return done(null, false, { message: 'Incorrect password.' });
+      }
+      return done(null, user);
+    });
+  }
+));
+//google auth2.0:
+passport.use(new GoogleStrategy(
+  {
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: 'http://localhost:3000/auth/google/home'
+  },
+  (accessToken, refreshToken, profile, cb) => {
+    User.findOrCreate({ googleId: profile.id }, (err, user) => {
+      return cb(err, user);
+    });
+  } 
+));
 
 const defaultList = new List({
   name: 'Quick List',
@@ -95,6 +143,7 @@ app.get('/sign-up', (req, res) => {
     date: date.getDate()
   })
 });
+
 //DYNAMIC ROUTING FOR OTHER LISTS.
 // app.get('/:listName', (req, res) => {
   //   let listTitle = _.capitalize(req.params.listName);
