baf80120e26f432629261354a70bf414f23304eb
PlayeredLC <playered.lc@gmail.com>
2021-02-01 18:34:56 -0300

    Small fix on google authentication.

--- app.js
+++ app.js
@@ -72,13 +72,14 @@ passport.use(new LocalStrategy(
   }
 ));
 //google auth2.0:
-passport.use(new GoogleStrategy({
+passport.use(new GoogleStrategy(
+{
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
   callbackURL: "http://localhost:3000/auth/google/home"
-},
-function(accessToken, refreshToken, profile, cb) {
-  User.findOrCreate({ googleId: profile.id }, function (err, user) {
+}, 
+(accessToken, refreshToken, profile, cb) => {
+  User.findOrCreate({ googleId: profile.id }, (err, user) => {
     return cb(err, user);
   });
 }
@@ -175,7 +176,7 @@ app.get('/auth/google',
 );
 
 app.get('/auth/google/home', 
-  passport.authenticate({failureRedirect: '/login'}),
+  passport.authenticate('google', {failureRedirect: '/login'}),
   (req, res) => {
     res.redirect('/');
   }
